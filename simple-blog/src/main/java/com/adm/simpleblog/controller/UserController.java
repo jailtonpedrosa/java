73c062a1c17459349421359d6e89c938f7a69ceb
jailtonpedrosa <55330749+jailtonpedrosa@users.noreply.github.com>
2024-02-21 14:30:12 -0300

    implementado exceptions

--- simple-blog/src/main/java/com/adm/simpleblog/controller/UserController.java
+++ simple-blog/src/main/java/com/adm/simpleblog/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.adm.simpleblog.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,9 +29,15 @@ public class UserController {
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = userService.findAll();
-		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x.getId(), x.getName(), x.getEmail())).collect(Collectors.toList());
-		
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x.getId(), x.getName(), x.getEmail())).collect(Collectors.toList());		
 		
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable("id") UUID id) {
+		Optional<User> user = userService.findById(id);
+		var userModel = user.get();		
+		return ResponseEntity.status(HttpStatus.OK).body(new UserDTO(userModel.getId(), userModel.getName(), userModel.getEmail()));
 	}
 }
--- simple-blog/src/main/java/com/adm/simpleblog/service/UserService.java
+++ simple-blog/src/main/java/com/adm/simpleblog/service/UserService.java
@@ -1,12 +1,15 @@
 package com.adm.simpleblog.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.adm.simpleblog.model.User;
 import com.adm.simpleblog.repository.UserRepository;
+import com.adm.simpleblog.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +19,15 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public Optional<User> findById(UUID id) {
+		Optional<User> user = userRepository.findById(id);
+		
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		
+		return user;
 	}
 }
