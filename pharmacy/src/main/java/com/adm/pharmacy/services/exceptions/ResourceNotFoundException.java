6ba57b685fc4a955adf4fd4dc55e399b7d74a3e7
jailtonpedrosa <55330749+jailtonpedrosa@users.noreply.github.com>
2024-03-09 17:24:38 -0300

    ...

--- pharmacy/src/main/java/com/adm/pharmacy/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.adm.pharmacy.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-	public ResourceNotFoundException(Object id) {
-		super("Remedy not found! Id " + id);
-	}
-}
